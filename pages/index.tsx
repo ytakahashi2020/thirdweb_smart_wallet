0a0abf4d26a4826ae0b070e94044830c2d2017cb
ytakahashi2020 <y.takahashi2020@gmail.com>
2023-06-06 06:47:09 +0900

    galleryの追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,39 @@
-import { ConnectWallet, Web3Button } from "@thirdweb-dev/react";
+import { ConnectWallet, Web3Button, useContract, useNFTs} from "@thirdweb-dev/react";
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
+import { NFTCard } from "../components/NFTCard";
+import { NFT_CONTRACT_ADDRESS } from "../const/addresses";
 
 const Home: NextPage = () => {
+  const { contract } = useContract(NFT_CONTRACT_ADDRESS);
+  const { data: nfts, isLoading: isLoadingNFTs } = useNFTs(
+    contract,
+    {
+      count: 30,
+      start: 0
+    }
+  )
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <ConnectWallet />
         aa
         <Web3Button 
-          contractAddress="0x1683B28f18b51bce492Be35D1EE9EA89f9544BE0"
+          contractAddress={NFT_CONTRACT_ADDRESS}
           action={(contract) => contract.erc721.claim(1)}
         >Claim
         </Web3Button>
+        <div>
+          <h3>Gallery</h3>
+          <div className={styles.NFTGrid}>
+          {!isLoadingNFTs && (
+            nfts?.map((nft, index) => (
+              <NFTCard key={index} nft={nft} />
+            ) 
+          ))}
+          </div>
+        </div>
+        
       </main>
     </div>
   );
